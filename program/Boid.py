164194b5d24216ef0f7c850ee5749a312acde8db
KeiDamy <65896722+KeiDamy@users.noreply.github.com>
2023-12-14 12:53:43 +0900

    Update Boid.py

--- program/Boid.py
+++ program/Boid.py
@@ -6,15 +6,18 @@ __author__ = 'Yokokawa Kotaro'
 __version__ = '1.0.0'
 __date__ = '2023/12/08(Created: 2023/12/08)'
 
+from math import cos, sin
 from program.Position import Position
 from program.Velocity import Velocity
 
 class Boid:
     """個々の生物を表すクラス"""
-    def __init__(self):
-        self.position = Position # 生物の位置
-        self.velocity = Velocity # 生物の速度と方向
+    def __init__(self, position, velocity):
+        self.position = position  # 生物の位置
+        self.velocity = velocity  # 生物の速度と方向
 
-    def updatePosition(position, velocity):
+    def updatePosition(self):
         """生物の位置を更新する"""
-        return 0
+        # 速度に基づいて位置を更新
+        self.position.x += self.velocity.speed * cos(self.velocity.direction)
+        self.position.y += self.velocity.speed * sin(self.velocity.direction)
