4ad044c8a2f2d7c5d023fb61b01ea98cd25dbc4a
KeiDamy <65896722+KeiDamy@users.noreply.github.com>
2023-12-14 12:53:23 +0900

    Update ApplicationController.py

--- program/ApplicationController.py
+++ program/ApplicationController.py
@@ -14,11 +14,19 @@ from program.View import View
 class ApplicationController:
     """コントローラー。他のコンポーネント間の調整を行う。"""
     def __init__(self):
-        self.handDetector = HandDetector()     #手の動きを検知するためのオブジェクト
-        self.boidSimulation = BoidSimulation() #Boidアルゴリズムに基づく生物群の動きをシミュレートするオブジェクト
-        self.rippleEffect = RippleEffect()     #波紋エフェクトを生成するオブジェクト
-        self.view = View()                     #システムの視覚的出力を担当するオブジェクト
+        self.handDetector = HandDetector()     
+        self.boidSimulation = BoidSimulation() 
+        self.rippleEffect = RippleEffect()     
+        self.view = View()                     
 
     def run(self):
         """プログラムの主要な実行を行う"""
-        return 0
+        while True:
+            # 手の検出
+            hands = self.handDetector.detectHands()
+            # Boidシミュレーションの更新
+            self.boidSimulation.simulateBoids()
+            # 波紋エフェクトの生成
+            self.rippleEffect.generateRipple()
+            # ビューの更新
+            self.view.updateDisplay()
