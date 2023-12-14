30efb79e6796a98c3e9a5dfb99e722cfd2c280b6
KeiDamy <65896722+KeiDamy@users.noreply.github.com>
2023-12-14 12:54:13 +0900

    Update RippleEffect.py

--- program/RippleEffect.py
+++ program/RippleEffect.py
@@ -11,8 +11,22 @@ from program.Position import Position
 class RippleEffect:
     """手の位置から波紋エフェクトを生成するクラス"""
     def __init__(self):
-        self.lastHandPosition = Position # 最後に検出された手の位置
+        self.lastHandPosition = Position()  # 最後に検出された手の位置
 
-    def generateRipple():
+    def generateRipple(self, handPosition):
         """波紋エフェクトを生成する"""
-        return 0
+        # 波紋エフェクトのパラメータを設定（半径、速度、色など）
+        ripple_params = {
+            "radius": 0,
+            "speed": 5,
+            "color": (255, 255, 255),
+            "opacity": 1.0
+        }
+
+        # handPosition から波紋の開始位置を設定
+        self.lastHandPosition = handPosition
+
+        # 波紋エフェクトを描画するロジックをここに実装
+        # （具体的な描画コードは、使用するグラフィックライブラリに依存）
+
+        return ripple_params  # 描画された波紋エフェクトを返す
